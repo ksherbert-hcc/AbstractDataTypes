fd79ac39c14470c139785068514d181d9d165fdb
Kyle Sherbert <ksherbert@hcc-nd.edu>
2026-05-04 22:46:30 -0400

    Give github actions permssion to write contents

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,13 @@
 public class Test {
     public static void main(String[] args) {
+        pass();
+    }
+
+    private static void pass() {
+        System.out.println("All tests pass.");
+        System.exit(0);
+    }
+    public static void fail() {
         System.err.println("Forcing a fail to test CI failure indicator.");
         System.exit(1);
     }
